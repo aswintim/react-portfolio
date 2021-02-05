fa5237195a200b45e009f53e4a75c8b53f684fd9
Aswin Timalsina <aswin.timalsina1@gmail.com>
2021-02-05 03:34:36 -0600

    commiting all

--- src/mainpage/Mainpage.js
+++ src/mainpage/Mainpage.js
@@ -12,8 +12,11 @@ import comeunity from '../assets/Projects/comeunity.PNG';
 import burger from '../assets/Projects/burger.PNG';
 
 class Mainpage extends Component {
+    
     render() {
-
+        var contact = (<div>
+            
+        </div>);
         return (
             <div className={styles.body1}>
                 <SideLink/>
@@ -72,12 +75,12 @@ class Mainpage extends Component {
                     // styling={"@media screen and (min-width: 1295px){.introRect{width: 507px}}"}
                     />
 
-<SectionSample
+                    <SectionSample
                     secId='contactMe'
                     title="CONTACT ME"
-                    hello='HELLO!'
+                    hello=''
                     content="I'm Aswin Timalsina. Currently, I'm studying Computer Science at University of Louisiana Monroe. The enormous possibilities of technology inspires me to learn and apply it. So blessed that I've got a laptop and internet. Let's make world a better place with technology! Please feel free to browse through these links and get to know me."
-                    cartoon='yes'
+                    cartoon=''
                    
                     />
 
