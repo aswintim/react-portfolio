a973b454590a60c93c911c10817643fa99800313
Aswin Timalsina <aswin.timalsina1@gmail.com>
2021-02-15 11:23:49 -0600

    fix: mobile responsive project and image

--- src/projects/ProjectModule.js
+++ src/projects/ProjectModule.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './project.module.css';
 
 
-const ProjectModule = props =>{
+class ProjectModule extends Component{
+    state={
+        projectMobile: ''
+    }
+
+    componentDidMount(){
+        window.addEventListener('resize', () => {
+            this.setState({
+                projectMobile: window.innerWidth < 1111
+            });
+        }, false);
+    }
+
+    render(){
+        const classy = this.state.projectMobile ? 'col-12' : 'col-lg-6';
     return(
         <div className={[styles.projectBox].join(' ')}>
             <div className='row'>
-                <div className='col-sm-12 col-md-12 col-lg-6' style={{textAlign: 'center'}}>
-                    <img src={props.image} className={styles.projectImage}></img>
+                <div className={['col-sm-12 col-md-12', classy].join(' ')} style={{textAlign: 'center'}}>
+                    <img src={this.props.image} className={styles.projectImage}></img>
                 </div>
-                <div className='col-sm-12 col-md-12 col-lg-6'>
+                <div className={['col-sm-12 col-md-12', classy].join(' ')}>
                     <div className='row'>
-                       <div className={['col', styles.titlee].join(' ')}>{props.titlee}</div>
+                       <div className={['col', styles.titlee].join(' ')}>{this.props.titlee}</div>
                    </div>
                    <div className='row'>
-                       <div className={['col', styles.description].join(' ')}>{props.description}</div>
+                       <div className={['col', styles.description].join(' ')}>{this.props.description}</div>
                    </div>
                    <div className='row'>
                        <div className={['col', styles.toolsBox].join(' ')}>
                            <span className={styles.toolsT}>Tools Used: </span>
-                           <span className={styles.tools}>{props.tools}</span>
+                           <span className={styles.tools}>{this.props.tools}</span>
                         </div>
                    </div>
                    <div className='row'>
-                       <div className={['col', styles.codeLive].join(' ')}>{props.code}</div>
+                       <div className={['col', styles.codeLive].join(' ')}>{this.props.code}</div>
                    </div>
                 </div>
             </div>
         </div>
     )
+    }
 }
 
 export default ProjectModule;
