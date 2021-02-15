8bc5030265df9e8734baf55071b84df7ec01ab73
Aswin Timalsina <aswin.timalsina1@gmail.com>
2021-02-15 10:14:41 -0600

    fix: position fixed

--- src/frontpage/Frontpage.js
+++ src/frontpage/Frontpage.js
@@ -17,7 +17,7 @@ class Frontpage extends Component {
                         fill-opacity="1"
                         d="M0,288L48,277.3C96,267,192,245,288,208C384,171,480,117,576,106.7C672,96,768,128,864,144C960,160,1056,160,1152,154.7C1248,149,1344,139,1392,133.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
                 </svg>
-                //greetings
+                {/* greetings */}
                 <div className={styles.greeting}>Namaste</div>
                 <div className={styles.namebarDiv}>
                     <img className={styles.namebar} src={nameBar} alt="Namebar svg"/>
