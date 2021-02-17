fcb81791c3ea154d54dacb9e130a5dc18d990f11
Aswin Timalsina <aswin.timalsina1@gmail.com>
2021-02-16 22:57:22 -0600

    not found and base url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Frontpage from './frontpage/Frontpage';
 import {Switch, Route} from 'react-router-dom';
 import Mainpage from './mainpage/Mainpage';
+import NotFound from './notfound/NotFound';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <Switch>
     <Route path='/' exact component={Frontpage} />
     <Route path='/mainpage' exact component={Mainpage} />
-
+    <Route component={NotFound} />
     
     
     </Switch>
