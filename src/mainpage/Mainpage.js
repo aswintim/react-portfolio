4760ddca021c56ab92adde0fbc5bc8bf146fc00b
Aswin Timalsina <aswin.timalsina1@gmail.com>
2021-11-29 14:58:25 -0600

    small changes

--- src/mainpage/Mainpage.js
+++ src/mainpage/Mainpage.js
@@ -29,7 +29,7 @@ class Mainpage extends Component {
                     hello='HELLO!'
                     content={
                     <div>I'm Aswin Timalsina. Currently, I'm studying Computer Science at University of Louisiana Monroe. The enormous possibilities of technology inspires me to learn and apply it. So blessed that I've got a laptop and internet. Let's make world a better place with technology!
-                    Please checkout my <a href="https://github.com/AswinTimalsina" target="_blank" className={styles.highlight}><span >Github</span></a> or <a href='#projects' className={styles.highlight}><span >projects</span></a> that I have worked on. Also, feel free to checkout my <a className={styles.highlight} href={resume} target='_blank'><span >resume</span></a> or send me an <a href='mailto:aswin.timalsina1@gmail.com' target='_blank' className={styles.highlight}><span >email</span></a>.</div>}
+                    Please checkout my <a href="https://github.com/AswinTimalsina" target="_blank" className={styles.highlight}><span >Github</span></a> or <a href='#projects' className={styles.highlight}><span >projects</span></a> that I have worked on. Also, feel free to checkout my <a className={styles.highlight} href='https://drive.google.com/file/d/1L-3WKeb08Rczcz7luNDzS7lL3lsTBgH-/view?usp=sharing' target='_blank'><span >resume</span></a> or send me an <a href='mailto:aswin.timalsina1@gmail.com' target='_blank' className={styles.highlight}><span >email</span></a>.</div>}
                     
                     cartoon='yes'
                     cartoonM='yes'/>
